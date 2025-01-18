7bf96c9e1f9e30a341ee0e77ad4b922343029e10
Tema <a.d.yakovenko@yandex.ru>
2025-01-18 19:03:59 +0300

    task1

---
+++ playmarket/task1/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=30)
+    balance = models.DecimalField(decimal_places=2, max_digits=30)
+    age = models.DecimalField(decimal_places=0, max_digits=3)
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(decimal_places=2, max_digits=20)
+    size = models.DecimalField(decimal_places=2, max_digits=20)
+    description = models.TextField()
+    age_limited = models.BooleanField()
+    buyer = models.ManyToManyField(Buyer, related_name='games')
